e612ca7d639449ccd3bd0c1912d0e92f2bb0faac
atarek017 <atarek017>
2022-09-25 11:36:45 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/posts_clean_architecher_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.posts_clean_architecher_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import 'core/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pasts App',
+      theme: appTheme,
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Posts"),
+      ),
+      body: const Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
