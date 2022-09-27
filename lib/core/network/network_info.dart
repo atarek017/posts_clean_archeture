c4ed4402a6e2404a9da4eec54e9808533e6b7435
atarek017 <atarek017>
2022-09-27 17:04:08 +0200

    add: add post repository implementer

---
+++ lib/core/network/network_info.dart
@@ -0,0 +1,15 @@
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+abstract class NetWorkInfo {
+  Future<bool> get isConnected;
+}
+
+
+class NetWorkInfoImpl implements NetWorkInfo{
+  final InternetConnectionChecker  _internetConnectionChecker;
+  NetWorkInfoImpl(this._internetConnectionChecker);
+
+  @override
+  Future<bool> get isConnected => _internetConnectionChecker.hasConnection;
+
+}
--- lib/features/posts/data/repositories/post_repository_impl.dart
+++ lib/features/posts/data/repositories/post_repository_impl.dart
@@ -1,41 +1,86 @@
 import 'package:dartz/dartz.dart';
+import 'package:posts_clean_architecher_app/core/error/exception.dart';
 import 'package:posts_clean_architecher_app/core/error/failure.dart';
+import 'package:posts_clean_architecher_app/features/posts/data/models/post_model.dart';
 import 'package:posts_clean_architecher_app/features/posts/domain/entities/post.dart';
 import 'package:posts_clean_architecher_app/features/posts/domain/repositories/post_repository.dart';
 
+import '../../../../core/network/network_info.dart';
 import '../data_sources/post_local_data_source.dart';
 import '../data_sources/post_remote_data_source.dart';
+
+typedef Future<Unit> DeleteOrUpdateOrUpdatePost();
 
 class PostRepositoryImpl implements PostRepository {
   final PostRemoteDateSource remoteDateSource;
   final PostLocalDateSource localDateSource;
+  final NetWorkInfo netWorkInfo;
 
   const PostRepositoryImpl(
-      {required this.remoteDateSource, required this.localDateSource});
+      {required this.remoteDateSource,
+      required this.localDateSource,
+      required this.netWorkInfo});
 
   @override
   Future<Either<Failure, List<Post>>> getAllPosts() async {
-    await remoteDateSource.getAllPosts();
-    await localDateSource.getCachedPosts();
+    if (await netWorkInfo.isConnected) {
+      try {
+        final remotePosts = await remoteDateSource.getAllPosts();
 
-    throw UnimplementedError();
+        localDateSource.cachePost(remotePosts);
+        return Right(remotePosts);
+      } on ServerException {
+        return left(ServerFailure());
+      }
+    } else {
+      try {
+        final localPosts = await localDateSource.getCachedPosts();
+
+        return Right(localPosts);
+      } on EmptyCacheException {
+        return left(EmptyCacheFailure());
+      }
+    }
   }
 
   @override
-  Future<Either<Failure, Unit>> addPost(Post post) {
-    // TODO: implement addPost
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> addPost(Post post) async {
+    final PostModel postModel =
+        PostModel(id: post.id, body: post.body, title: post.title);
+
+    return await _getMessage(() {
+      return remoteDateSource.addPost(postModel);
+    });
   }
 
   @override
-  Future<Either<Failure, Unit>> deletePosts(int id) {
-    // TODO: implement deletePosts
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> deletePosts(int postId) async {
+    return await _getMessage(() {
+      return remoteDateSource.deletePost(postId);
+    });
   }
 
   @override
-  Future<Either<Failure, Unit>> updatePost(Post post) {
-    // TODO: implement updatePost
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> updatePost(Post post) async {
+    final PostModel postModel =
+        PostModel(id: post.id, body: post.body, title: post.title);
+
+    return await _getMessage(() {
+      return remoteDateSource.updatePost(postModel);
+    });
+  }
+
+  Future<Either<Failure, Unit>> _getMessage(
+      DeleteOrUpdateOrUpdatePost function) async {
+    if (await netWorkInfo.isConnected) {
+      try {
+        await function();
+        return const Right(unit);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      return Left(OfflineFailure());
+    }
   }
 }
